6185d679f32649e51b0b8666801ae3b0742a4a11
OleksandrTkachuk3241 <119621369+OleksandrTkachuk3241@users.noreply.github.com>
2022-12-16 16:45:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-print("I love Python " * 42)
-
-age_in_month = 252
-age_in_years = 252 // 12
-my_age = ("Му name is Oleksandr I’m " + str(age_in_years) + " years old")
-print(my_age)
-
-x = 1
-c = x > 0
-print(c)
-v = x >= 5
-print(v)
-b = x < 5
-print(b)
-n = x <= -2
-print(n)
-m = x != 2
-print(m)
-
-a = 2
-b = 5
-c = 6
-d = str(a) + str(b) + str(c)
-print(d)
+i = input("Введите текст: ")
+if i.isdigit():
+    i = int(i)
+    if i % 2 == 0:
+        print("Вы ввели парное число")
+    else:
+        print("Вы ввели не парное число")
+else:
+    print(f'Вы ввели слово длинною в {len(i)} символа(ов)')
