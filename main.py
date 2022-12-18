5d81852451f5804dba45103b927ffff2d7c2760b
OleksandrTkachuk3241 <119621369+OleksandrTkachuk3241@users.noreply.github.com>
2022-12-18 21:49:55 +0100

    Add files via upload
    DZ-3 (1, 2)

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-print("I love Python " * 42)
+import time
 
-age_in_month = 252
-age_in_years = 252 // 12
-my_age = ("Му name is Oleksandr I’m " + str(age_in_years) + " years old")
-print(my_age)
-
-x = 1
-c = x > 0
-print(c)
-v = x >= 5
-print(v)
-b = x < 5
-print(b)
-n = x <= -2
-print(n)
-m = x != 2
-print(m)
-
-a = 2
-b = 5
-c = 6
-d = str(a) + str(b) + str(c)
-print(d)
+while True:
+    print('"I love Puthon"')
+    time.sleep(4.2)
